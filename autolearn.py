4b575f7f4c07f25e3952dd69e29a7076e3642d58
Eric Pedley <ericpedley@gmail.com>
2022-05-28 19:41:47 -0700

    renamed regression to autolearn and added classification

--- autolearn.py
+++ autolearn.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas.core.series import Series#for type hint that helps intellisense
 import autosklearn.regression
+import autosklearn.classification
 import sklearn.model_selection
 
 def run_regression_ensemble(X: Series, y: Series) -> autosklearn.regression.AutoSklearnRegressor:
@@ -8,6 +9,17 @@ def run_regression_ensemble(X: Series, y: Series) -> autosklearn.regression.Auto
                 sklearn.model_selection.train_test_split(X, y, random_state=1)
 
     automl = autosklearn.regression.AutoSklearnRegressor(
+                time_left_for_this_task=120,
+                    per_run_time_limit=30,
+            )
+    automl.fit(X_train, y_train)
+    return automl
+
+def run_classification_ensemble(X: Series, y: Series) -> autosklearn.classification.AutoSklearnClassifier:
+    X_train, X_test, y_train, y_test = \
+                sklearn.model_selection.train_test_split(X, y, random_state=1)
+
+    automl = autosklearn.classification.AutoSklearnClassifier(
                 time_left_for_this_task=120,
                     per_run_time_limit=30,
             )
