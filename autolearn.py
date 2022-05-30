1384e1aa2486d86782ca5ee0d956a5e6763f1c56
Eric Pedley <ericpedley@gmail.com>
2022-05-29 21:01:21 -0700

    removed train/test splitter

--- autolearn.py
+++ autolearn.py
@@ -8,25 +8,25 @@ SECONDS_PER_DATASET = 60
 SECONDS_PER_MODEL = 30
 
 def run_regression_ensemble(X: Series, y: Series) -> autosklearn.regression.AutoSklearnRegressor:
-    X_train, X_test, y_train, y_test = \
-                sklearn.model_selection.train_test_split(X, y, random_state=1)
+    # X_train, X_test, y_train, y_test = \
+    #             sklearn.model_selection.train_test_split(X, y, random_state=1)
 
     automl = autosklearn.regression.AutoSklearnRegressor(
                 time_left_for_this_task=SECONDS_PER_DATASET,
                     per_run_time_limit=SECONDS_PER_MODEL,
                     scoring_functions=[autosklearn.metrics.mean_squared_error]
             )
-    automl.fit(X_train, y_train)
+    automl.fit(X, y)
     return automl
 
 def run_classification_ensemble(X: Series, y: Series) -> autosklearn.classification.AutoSklearnClassifier:
-    X_train, X_test, y_train, y_test = \
-                sklearn.model_selection.train_test_split(X, y, random_state=1)
+    # X_train, X_test, y_train, y_test = \
+    #             sklearn.model_selection.train_test_split(X, y, random_state=1)
 
     automl = autosklearn.classification.AutoSklearnClassifier(
                 time_left_for_this_task=SECONDS_PER_DATASET,
                     per_run_time_limit=SECONDS_PER_MODEL,
                     scoring_functions =[autosklearn.metrics.roc_auc]
             )
-    automl.fit(X_train, y_train)
+    automl.fit(X, y)
     return automl
